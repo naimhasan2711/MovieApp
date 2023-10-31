a57e1e02b041533c2f7a25961f81ec723b5ab7d3
Nakibul Hassan <nakibhasan2711@gmail.com>
2023-10-31 12:45:59 +0600

    - Added Genre Adapter

--- app/src/main/java/com/nakibul/android/movieapp/MovieDetailFragment.kt
+++ app/src/main/java/com/nakibul/android/movieapp/MovieDetailFragment.kt
@@ -5,17 +5,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.nakibul.android.movieapp.databinding.FragmentMovieDetailBinding
+import com.nakibul.android.movieapp.databinding.FragmentMovieListBinding
+import com.nakibul.android.movieapp.presentation.pages.MovieDetailViewModel
 
 
 class MovieDetailFragment : Fragment() {
 
+    private lateinit var binding :FragmentMovieListBinding
+    private val movieDetailsViewModel: MovieDetailViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_detail, container, false)
+        binding = FragmentMovieListBinding.inflate(inflater,container,false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val movieId = requireArguments().getInt(BUNDLE_KEY_MOVIE_ID)
+        movieDetailsViewModel.getMovieDetailsById(id)
+        observeData()
+    }
+
+    private fun observeData() {
+        TODO("Not yet implemented")
+    }
+
 
     companion object {
         const val BUNDLE_KEY_MOVIE_ID = "movie_id"
---
+++ app/src/main/java/com/nakibul/android/movieapp/presentation/adapters/GenreAdapter.kt
@@ -0,0 +1,36 @@
+package com.nakibul.android.movieapp.presentation.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.nakibul.android.movieapp.R
+import com.nakibul.android.movieapp.domain.models.Genre
+
+class GenreAdapter(private val genres: List<Genre>) :
+    RecyclerView.Adapter<GenreAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val genreName: TextView = itemView.findViewById(R.id.genresItemName)
+
+        fun bind(item: Genre) {
+            genreName.text = item.name
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_genre, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return genres.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(genres[position])
+    }
+}
